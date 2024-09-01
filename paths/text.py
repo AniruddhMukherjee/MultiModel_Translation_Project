8d099d16210760e5c680e24930f5c5a91cfc50c0
Aniruddh Mukherjee <97076878+AniruddhMukherjee@users.noreply.github.com>
2024-09-01 10:04:28 +0000

    chnage auto detect

--- paths/text.py
+++ paths/text.py
@@ -1,32 +1,40 @@
 import streamlit as st
 from st_audiorec import st_audiorec
-import googletrans
+from googletrans import Translator, LANGUAGES
 from gtts import gTTS
 import os
 import speech_recognition as sr
 import io
 
 def translate_text():
-    st.title("Text Translator")
+    st.title("Text Translator with Auto Language Detection")
 
-    input_lang = st.selectbox("Select input language:", list(googletrans.LANGUAGES.values()), index=list(googletrans.LANGUAGES.values()).index('english'))
-    output_lang = st.selectbox("Select output language:", list(googletrans.LANGUAGES.values()), index=list(googletrans.LANGUAGES.values()).index('german'))
+    # Only select output language
+    output_lang = st.selectbox("Select output language:", list(LANGUAGES.values()), index=list(LANGUAGES.values()).index('english'))
 
-    input_text = st.text_area("Enter text to translate:", height=150)
+    input_text = st.text_area("Enter text to translate (language will be auto-detected):", height=150)
     
     if input_text:
-        st.subheader(f"input : {input_text}")
+        st.subheader(f"Input text: {input_text}")
 
     if st.button("Translate"):
         if input_text:
-            translator = googletrans.Translator()
-            input_lang_code = list(googletrans.LANGUAGES.keys())[list(googletrans.LANGUAGES.values()).index(input_lang)]
-            output_lang_code = list(googletrans.LANGUAGES.keys())[list(googletrans.LANGUAGES.values()).index(output_lang)]
+            translator = Translator()
             
-            translation = translator.translate(input_text, src=input_lang_code, dest=output_lang_code)
+            # Detect input language
+            detection = translator.detect(input_text)
+            detected_lang = LANGUAGES.get(detection.lang, 'Unknown')
+            st.write(f"Detected input language: {detected_lang}")
+
+            # Get output language code
+            output_lang_code = list(LANGUAGES.keys())[list(LANGUAGES.values()).index(output_lang)]
+            
+            # Translate
+            translation = translator.translate(input_text, dest=output_lang_code)
             
             st.subheader(f"Translation: {translation.text}")
 
+            # Text-to-speech
             tts = gTTS(translation.text, lang=output_lang_code)
             tts.save("translation.mp3")
             
@@ -35,7 +43,7 @@ def translate_text():
             # Clean up the audio file
             os.remove("translation.mp3")
         else:
-            st.warning("Please enter some text or record speech to translate.")
+            st.warning("Please enter some text to translate.")
 
 if __name__ == '__main__':
     translate_text()
